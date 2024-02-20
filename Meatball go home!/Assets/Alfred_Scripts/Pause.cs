032b1b4f3a12d340c25266368bf1c6c0adc3cc51
Alfred Ramirez <alfrednicramirez@gmail.com>
2024-02-20 16:07:20 -0500

    Menus
    - Added all menus:
      - Main
        - Quit button functional
      - About
        - How To Play
        - Credits
      - Options
        - Volume slider fully functional
        - Mode toggles nonfunctional, will
           change once we're solid on modes
      - Pause
        - Functional game
           pausing/unpausing on ESC and
           Resume
    
    TBA:
    - Data saving over scenes (e.g. volume slider saves same set value on Main Menu and in game)
    - BG Music
    - BG Image
    - Custom Text Font

---
+++ Meatball go home!/Assets/Alfred_Scripts/Pause.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
+    public 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Paused()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        //SceneManager.LoadScene(""); whatever Alfred's main menu scene is defined as
+        Debug.Log("Loading Menu...");
+    }
+}
